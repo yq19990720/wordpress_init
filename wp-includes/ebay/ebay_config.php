a063c4209eb5224eff307bc591115473e98d60a0
那个憨憨 <73150359+yq19990720@users.noreply.github.com>
2022-08-01 13:57:46 +0800

    test
    rt

---
+++ wp-includes/ebay/ebay_config.php
@@ -0,0 +1,23 @@
+<?php
+//ebay
+define("Api_url","");//apiUrl
+define("Api_key","");//apiKey
+define("App_id","");//apiId
+define("Api_Src","ebay");//Api_Src
+define("Api_Extra","us");//Api_Extra
+
+//list
+define('LIST_PAGE_META_0', 'codibook');
+define('LIST_PAGE_META_1', 'best sell');
+define('LIST_PAGE_META_2', 'outlet store');
+
+
+//language-----en是默认语言请不要删除,添加多个语言请用逗号隔开比如array('en','zh_CN')
+define('LANGUAGE_SETTINGS',array('en','zh-CN'));
+
+// Home Page Only:
+define('HOME_PAGE_META_DESCRIPTION', '');
+define('HOME_PAGE_META_KEYWORDS', '');
+
+// NOTE: If HOME_PAGE_TITLE is left blank (default) then TITLE and SITE_TAGLINE will be used instead.
+define('HOME_PAGE_TITLE', 'hello word'); // usually best left blank
---
+++ wp-includes/ebay/session.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Session控制类
+ */
+class Session{
+
+    /**
+     * 设置session
+     * @param String $name   session name
+     * @param Mixed  $data   session data
+     * @param Int    $expire 超时时间(秒)
+     */
+    public static function set($name, $data, $expire=null){
+        if($_SESSION[$name]){
+            return false;
+        }
+        $session_data = array();
+        $session_data['data'] = $data;
+        if(!$expire){
+            $expire = SESSION_TIME;//default time
+        }
+        $session_data['expire'] = time()+$expire;
+        $_SESSION[$name] = $session_data;
+    }
+
+    /**
+     * 读取session
+     * @param  String $name  session name
+     * @return Mixed
+     */
+    public static function get($name){
+        if(isset($_SESSION[$name])){
+            if($_SESSION[$name]['expire']>time()){
+                return $_SESSION[$name]['data'];
+            }else{
+                self::clear($name);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 清除session
+     * @param  String  $name  session name
+     */
+    public static function clear($name){
+        unset($_SESSION[$name]);
+    }
+
+}
+?>
